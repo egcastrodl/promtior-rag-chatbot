92d93a9712940b70c51caaa25509ffcf2e1d9959
egcastrodl <e.g.castro.dl@gmail.com>
2026-02-06 15:15:51 -0300

    Update server.py

--- server.py
+++ server.py
@@ -23,4 +23,7 @@ add_routes(
 # Run with: python server.py
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    import os
+    
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
